3f407f6290c9626771b3ef6e8f37a76dc39a01c2
Vishesh007-creator <turbofacts007@gmail.com>
2026-09-19 11:11:16 +0530

    Added target job feature

--- app.py
+++ app.py
@@ -1,48 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Home Page
 @app.route("/")
 def home():
-    return render_template("index.html")
-
-
-# Career Selection Page
-@app.route("/career")
-def career():
-    return render_template("career.html")
-
-
-# Assessment Page
-@app.route("/assessment")
-def assessment():
-    return render_template("assessment.html")
-
-
-# Dashboard Page
-@app.route("/dashboard", methods=["POST"])
-def dashboard():
-
-    skills = ["python", "sql", "numpy", "pandas", "ml"]
-
-    score = 0
-    missing = []
-
-    for skill in skills:
-
-        if request.form[skill] == "Yes":
-            score += 20
-
-        else:
-            missing.append(skill.upper())
-
-    return render_template(
-        "dashboard.html",
-        score=score,
-        missing=missing
-    )
-
+    return render_template("target.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
